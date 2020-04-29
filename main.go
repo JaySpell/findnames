fd281f2896dbc8d0c110ed2db0877fdc6b91ca22
Jeremy Spell <jspell@gmail.com>
2020-04-29 10:14:29 -0500

    Before implementing func

--- main.go
+++ main.go
@@ -26,29 +26,32 @@ func main() {
 	file.Close()
 
 	tlsConfig := tls.Config{InsecureSkipVerify: true} //Set TLS to ignore cert errors
-	listIPName := make(map[string][]string)           //Map to hold the IP Address and DNS names
+	listIPName := [][]string{}                        //Map to hold the IP Address and DNS names
 	for _, ipaddress := range txtlines {              //Loop through list of IP and output certname associated
 		ipWithPort := ipaddress + ":443"                     //Set IP & port for passing
 		conn, err := tls.Dial("tcp", ipWithPort, &tlsConfig) //Make TLS call
-		fmt.Println(err)
+		//fmt.Println(err)
 
 		if err != nil {
 			log.Print(err)
 			continue
 		} else {
 			certChain := conn.ConnectionState().PeerCertificates //Pull entire certificate chain
-			cert := certChain[0]                                 //Set certificate to server based certificate
-			listIPName[ipaddress] = cert.DNSNames                //Add the full list of names associated with cert to map
+			cert := certChain[0]
+			ip := []string{ipaddress, cert.DNSNames[0]}
+			listIPName = append(listIPName, ip) //Add the full list of names associated with cert to map
 		}
+		fmt.Print(listIPName)
 	}
 	fmt.Print(listIPName)
+	fmt.Println(err)
 }
 
-//outputcsv will take hash map of IP & addresses and output a CSV 
+//outputcsv will take hash map of IP & addresses and output a CSV
 //representation of values within the passed map
-func outputcsv(listIPName *map[string][]string) string, error {
+/*func outputcsv(listIPName *map[string][]string) string, error {
 	for k, v := range listIPName {
 
 	}
 	return csvIPName, err
-}
+}*/
