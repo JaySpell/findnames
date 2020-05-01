f208d9460181649cc30e7f367f1a8e5982d3bea0
Jeremy Spell <jspell@gmail.com>
2020-05-01 17:51:53 -0500

    Switched to flag instead of parsing args..

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"crypto/tls"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -15,31 +16,17 @@ DNS name associated with the certificate that is returned.
 **************************************************************************/
 
 func main() {
-	//Setup variables for program
-	var inputfile, outputfile string
-	args := os.Args[1:] //Get command line arguments
-	if args != nil {    //Determine if arguments for inputfile or outputfile
-		for i := 0; i < len(args); i++ {
-			if args[i] == "-i" {
-				inputfile = args[i+1]
-			} else if args[i] == "-o" {
-				outputfile = args[i+1]
-			} else if args[i] == "-s" {
-				outputfile = "SCREENPRINTONLY"
-			} else if args[i] == "-h" || args[i] == "--h" || args[i] == "-help" {
-				fmt.Println("Findnames outputs the first certificate in a chain associated with an IP address..")
-				fmt.Println("-i Input filename ")
-				fmt.Println("-o Output filename")
-				fmt.Println("-s Screen print results")
-				os.Exit(3)
-			}
-		}
-	}
-	if inputfile == "" || outputfile == "" {
-		inputfile, outputfile = getfilenames() //If inputfile or outputfile not set them from input
-	}
-	fmt.Println("files = ", inputfile, outputfile)
-	file, err := os.Open(inputfile) //Open file with IP to pull cert information for
+	//Setup variables
+	inputFile := flag.String("i", "filename", "input filename")
+	outputFile := flag.String("o", "filename", "output filename")
+	screenOnly := flag.Bool("s", false, "print to screen")
+	flag.Parse()
+
+	fmt.Println(*inputFile)
+	fmt.Println(*outputFile)
+	fmt.Println(*screenOnly)
+
+	file, err := os.Open(*inputFile) //Open file with IP to pull cert information for
 
 	if err != nil {
 		log.Fatalf("failed opening file: %s", err)
@@ -78,7 +65,9 @@ func main() {
 			}
 		}
 	}
-	outputcsv(&listIPName)
+
+	//fmt.Println(listIPName)
+	outputcsv(&listIPName, outputFile, screenOnly)
 }
 
 //addtoslice will take a slice and add another slice to it
@@ -94,18 +83,32 @@ func addtoslice(sarr *[][]string, s []string) {
 
 //outputcsv will take hash map of IP & addresses and output a CSV
 //representation of values within the passed map
-func outputcsv(sarr *[][]string) error {
-	err := fmt.Errorf("Could not write to file") //Send error if unable to write
-	w := csv.NewWriter(os.Stdout)                //Write output to console
-	w.WriteAll(*sarr)                            // calls Flush internally
-	if err := w.Error(); err != nil {
-		log.Fatalln("error writing csv:", err)
+func outputcsv(sarr *[][]string, of *string, s *bool) error {
+	err := fmt.Errorf("error in outputcsv") //Send error if unable to write
+	if *s {
+		w := csv.NewWriter(os.Stdout) //Write output to console
+		w.WriteAll(*sarr)             // calls Flush internally
+		if err := w.Error(); err != nil {
+			log.Fatalln("error writing csv:", err)
+		}
+	} else {
+		csvFile, err := os.Create(*of)
+		if err != nil {
+			log.Fatalf("failed creating file: %s", err)
+		}
+		csvwriter := csv.NewWriter(csvFile)
+		for _, row := range *sarr {
+			_ = csvwriter.Write(row)
+		}
+		csvwriter.Flush()
+		csvFile.Close()
+
 	}
 	return err
 }
 
 /*getfilenames will scan from command line and set variables for
-input / output files and return them in as two variables inputfile & outputfile*/
+input / output files and return them in as two variables inputfile & outputFile*/
 func getfilenames() (string, string) {
 	fmt.Println("Enter name of input file: ")
 	inputfile := bufio.NewScanner(os.Stdin) //Get input file name
